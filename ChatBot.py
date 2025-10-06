e95dfba272a2af654c7854fd49fff01e97fa69ac
abhirup2565 <abhirup2565@gmail.com>
2025-10-06 18:59:19 +0530

    persistent state manintained

---
+++ ChatBot.py
@@ -0,0 +1,55 @@
+import getpass
+import os
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, AIMessage
+
+load_dotenv()
+if not os.environ.get("GOOGLE_API_KEY"):
+  os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
+
+#Providing context history
+# model.invoke(
+#     [
+#         HumanMessage(content="Hi! I'm Bob"),
+#         AIMessage(content="Hello Bob! How can I assist you today?"),
+#         HumanMessage(content="What's my name?"),
+#     ]
+# )
+
+#Using langraph that has in build persistent layer
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
+
+# Define a new graph
+workflow = StateGraph(state_schema=MessagesState)
+
+#config
+config = {"configurable": {"thread_id": "abc123"}}
+# Define the function that calls the model
+def call_model(state: MessagesState):
+    response = model.invoke(state["messages"])
+    return {"messages": response}
+
+
+# Define the (single) node in the graph
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+# Add memory
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
+#Starting convo
+query = "Hi! I'm Bob."
+input_messages = [HumanMessage(query)]
+output = app.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print() 
+
+query = "What's my name?"
+input_messages = [HumanMessage(query)]
+output = app.invoke({"messages": input_messages}, config)
+output["messages"][-1].pretty_print()
