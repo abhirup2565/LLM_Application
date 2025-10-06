8701b5b1a9705d5445544676d3e9771d58ce5794
abhirup2565 <abhirup2565@gmail.com>
2025-10-06 18:03:41 +0530

    using template

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
@@ -11,11 +12,22 @@ if not os.environ.get("GOOGLE_API_KEY"):
 
 model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
 
-messages = [
-    SystemMessage(content="Translate the following from English into Italian"),
-    HumanMessage(content="hi!"),
-]
+#without template
+# messages = [
+#     SystemMessage(content="Translate the following from English into Italian"),
+#     HumanMessage(content="hi!"),
+# ]
+# model.invoke(messages)
+# for token in model.stream(messages):
+#     print(token.content, end="|")
 
-model.invoke(messages)
-for token in model.stream(messages):
-    print(token.content, end="|")
+#with template
+system_template = "Translate the following from English into {language}"
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+
+prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
+response = model.invoke(prompt)
+print(response)
+print(response.content)
